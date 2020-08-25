688de477f05de93b0cfb39d2339aa1ee71dfd7a0
mengjiann <mengjiannlee@gmail.com>
2020-08-25 23:22:37 +0800

    #32 Added end to end test for EnunciateTestsGenerator

---
+++ src/test/java/dk/jyskebank/tools/enunciate/modules/openapi/confignamespacewithprefix/DataXmlEnum.java
@@ -0,0 +1,9 @@
+package dk.jyskebank.tools.enunciate.modules.openapi.confignamespacewithprefix;
+
+import javax.xml.bind.annotation.XmlEnum;
+
+@XmlEnum
+public enum DataXmlEnum {
+  GOOD,
+  MORNING;
+}
